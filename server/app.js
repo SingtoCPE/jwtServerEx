a8a64885022f0a5647e1e28e0f454a7b9b686095
thitipat <thitipat.cpe18@hotmail.com>
2019-09-16 16:27:37 +0700

    initial project.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
 const mysql = require("mysql");
-const jwt = require("jsonwebtoken");
-const fs = require("fs");
+const path = require("path");
 const app = express();
-const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(fileUpload());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
   res.header(
@@ -15,45 +19,64 @@ app.use(function(req, res, next) {
   next();
 });
 
+const port = 3000;
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "singto11442525",
   database: "loginDB"
 });
-
-// and a /readme endpoint which will be open for the world to see
-app.get("/readme", (req, res) => {
-  res.json({ message: "This is open to the world!" });
+app.get("/employee", (req, res) => {
+  let sql = "SELECT * FROM loginDB.tableDB;";
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
 });
-
-app.get("/jwt", (req, res) => {
-  let privateKey = fs.readFileSync("./private.pem", "utf8");
-  let token = jwt.sign({ body: "stuff" }, privateKey, { algorithm: "HS256" });
-  res.send(token);
-});
-
-app.get("/secret", isAuthenticated, (req, res) => {
-  res.json({ message: "THIS IS SUPER SECRET, DO NOT SHARE!" });
-});
-
-function isAuthenticated(req, res, next) {
-  if (typeof req.headers.authorization !== "undefined") {
-    let token = req.headers.authorization;
-    let privateKey = fs.readFileSync("./private.pem", "utf8");
-    jwt.verify(token, privateKey, { algorithm: "HS256" }, (err, user) => {
-      if (err) {
-        res.status(500).json({ error: "Not Authorized" });
-        throw new Error("Not Authorized");
+//----------------------------------------------------------
+app.post("/employee/login", (req, res) => {
+  let sql =
+    "SELECT user,password FROM loginDB.tableDB WHERE user='singto1144';";
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    const userUI = req.body.user;
+    const passUI = req.body.password;
+    const userDB = results.map(results => results.user);
+    const passDB = results.map(results => results.password);
+    console.log("user:", userDB);
+    console.log("pass:", passDB);
+    console.log("userByClient:", userUI);
+    for (const o of userDB) {
+      if (userUI === userDB) {
+        if (passUI === passDB) {
+          res.status(201).json("login success");
+        }
       }
-      return next();
-    });
-  } else {
-    res.status(500).json({ error: "Not Authorized" });
-    throw new Error("Not Authorized");
-  }
-}
+    }
+    res.status(403).json("login failed");
+  });
+});
+// ---------------------------------------------------------
+app.post("/employee/del", (req, res) => {
+  console.log({ id: req.body.id });
+  let sql = `DELETE FROM loginDB.tableDB WHERE id=${req.body.id};`;
+
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+// ---------------------------------------------------------
+app.post("/employee/add", (req, res) => {
+  let sql = `INSERT INTO loginDB.tableDB (user, password, name) VALUES ('${req.body.user}', 
+  '${req.body.password}', '${req.body.name}');`;
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+//------------------------------------------------------------
 
 app.listen(port, () => {
-  console.log("Project running on port:", port);
+  console.log(`Server running on port: ${port}`);
 });
